55e4041ad1658d9e2109dedc9cd795a71e2e45d6
Hoon Wee <mrgravity817@gmail.com>
2025-10-13 21:47:37 +0900

    feat(render): add map and entity rendering systems
    - Implemented `map_render` and `entity_render` systems for drawing the map and entities.
    - Integrated new systems into the scheduler.
    - Replaced TODO in main state update with actual draw buffer rendering.
    - Ensured rendering order by submitting draw batches with priorities.

--- src/systems.rs
+++ src/systems.rs
@@ -1,9 +1,13 @@
+mod entity_render;
+mod map_render;
 mod player_input;
 
-use crate::{prelude::*, systems::player_input::player_input_system};
+use crate::prelude::*;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(player_input_system())
+        .add_system(player_input::player_input_system())
+        .add_system(map_render::map_render_system())
+        .add_system(entity_render::entity_render_system())
         .build()
 }
